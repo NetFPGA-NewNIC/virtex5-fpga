340d7a27e1ed8195f3342ca59589d4f24d081f12
forconesi <forcoxr@gmail.com>
2014-12-29 18:32:00 +0000

    test if it works

--- impact.cmd
+++ impact.cmd
@@ -2,6 +2,6 @@ setMode -bs
 setCable -port auto
 Identify -inferir 
 identifyMPM 
-assignFile -p 1 -file "./bitfile/my_top.bit"
+assignFile -p 1 -file "./bitfile/top.bit"
 Program -p 1 
 quit
--- program_fpga.sh
+++ program_fpga.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 source /opt/Xilinx/14.6/ISE_DS/settings64.sh
 impact -batch impact.cmd
